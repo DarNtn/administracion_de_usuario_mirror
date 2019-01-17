add719521515595e6b4894449127c7603d163b7f
boscoand <boscoand12@gmail.com[branch envio_correo]>
2019-01-17 00:59:40 -0500

    [Fixes #159610781] Enviar correo cuando contrasena ha sido cambiada

--- funciones/contrasena/contrasenaModelo.php
+++ funciones/contrasena/contrasenaModelo.php
@@ -1,7 +1,7 @@
 <?php
 
 require_once '../conexion/php_conexion.php';
-
+require_once('../mail/mail.php');
 class Contrasena extends php_conexion {
 
     public function cambiarContrasena($username, $tipoUsuario, $contrasenaAntigua, $contrasenaNueva){
@@ -9,8 +9,17 @@ class Contrasena extends php_conexion {
         if($tipoUsuario == "a"){
             
             $res = $this->realizarConsulta("
-                select usuario_id from usuario where tipo='a' and usuario='$username' and clave='$contrasenaAntigua';
+                select usuario_id 
+                from usuario 
+                where tipo='a' and usuario='$username' and clave='$contrasenaAntigua';
             ");
+            if($res != null){
+                $usuario_id = $res[0]['usuario_id'];
+                $correo = $this->realizarConsulta("
+                    select correo from administrador where usuarios_usuario_id=$usuario_id;
+                ");
+                $correo = $correo[0]['correo'];
+            }
         }
         
         if($tipoUsuario == "p"){
@@ -20,6 +29,14 @@ class Contrasena extends php_conexion {
                 from usuario 
                 where tipo='p' and usuario='$username' and clave='$contrasenaAntigua';
             ");
+        
+            if($res != null){
+                $usuario_id = $res[0]['usuario_id'];
+                $correo = $this->realizarConsulta("
+                    select mail from personal where usuario_id=$usuario_id;
+                ");
+                $correo = $correo[0]['mail'];
+            }
             
         }
         
@@ -30,6 +47,12 @@ class Contrasena extends php_conexion {
                 set clave='$contrasenaNueva'
                 where usuario_id=$usuario_id;
             ");
+            
+            if($correo!=null){
+                $mail = new mailEnviar();
+                list($estado, $mensaje) = $mail->enviarMensaje($correo, "Cambio de contrasena", "Buenas tardes. Le informamos que su contrasena ha sido cambiada.");     
+            }
+            
             return $this->mensajes('success', 'Contraseña modificada con éxito');
         }else{
             return $this->mensajes('error', 'Contraseña incorrecta'); 
